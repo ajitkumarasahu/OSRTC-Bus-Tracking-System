e111fbe7f9c20c1447a1e1067f8d9902cac050fb
Ajit Kumar Sahu <sahuajitkumara@gmail.com>
2026-01-20 09:47:06 +0530

    comment line Explain the code

--- src/main/java/com/osrtc/bustracking/config/ApplicationConfig.java
+++ src/main/java/com/osrtc/bustracking/config/ApplicationConfig.java
@@ -1,14 +1,20 @@
-
+// Package declaration – groups this class under the config package
 package com.osrtc.bustracking.config;
 
+// Import Jersey's ResourceConfig class used to configure REST resources
 import org.glassfish.jersey.server.ResourceConfig;
+
+// Import annotation to define the base URI for REST APIs
 import javax.ws.rs.ApplicationPath;
 
+// Defines the base path for all REST endpoints (e.g., /api/endpoint)
 @ApplicationPath("/api")
 public class ApplicationConfig extends ResourceConfig {
 
+    // Constructor is called when the application starts
     public ApplicationConfig() {
-        // Scan all REST controllers
+
+        // Tells Jersey to scan this package for REST controllers (@Path classes)
         packages("com.osrtc.bustracking.controller");
     }
 }
